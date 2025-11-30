0854b68d9922a6dbb4314d9a9828ba6d6b67c0a6
prakashsingh62 <prakashkumarsing@gmail.com>
2025-11-30 18:54:16 +0530

    Delete app directory

--- app/backend_api.py
+++
@@ -1,5 +0,0 @@
-
-from fastapi import FastAPI
-from app.api.router_main import router as main_router
-def register_routes(app:FastAPI):
-    app.include_router(main_router)
--- app/main.py
+++
@@ -1,5 +0,0 @@
-
-from fastapi import FastAPI
-from app.backend_api import register_routes
-app = FastAPI()
-register_routes(app)
