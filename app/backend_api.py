e0114c57e499f63fee0939d7f8bc41aff8436eaa
prakashsingh62 <prakashkumarsing@gmail.com>
2025-11-30 23:00:08 +0530

    Update backend_api.py

--- app/backend_api.py
+++ app/backend_api.py
@@ -1,11 +1,16 @@
-from app.api.router_test import router as test_router
 from fastapi import FastAPI
+
+# Existing routers
 from app.api.router_main import router as main_router
 from app.api.router_rfq import router as rfq_router
 from app.api.router_mail import router as mail_router
 from app.api.router_jobs import router as jobs_router
 
-def register_routes(app:FastAPI):
+# Test router
+from app.api.router_test import router as test_router
+
+
+def register_routes(app: FastAPI):
     app.include_router(main_router)
     app.include_router(rfq_router)
     app.include_router(mail_router)
